1718ac25d13fd7457ecf03f3f9a7398b21eb55e6
HitaloYo <76579977+HitaloYo@users.noreply.github.com>
2023-10-31 02:16:23 -0300

    Update Beecrownd.py

--- Beecrownd.py
+++ Beecrownd.py
@@ -214,7 +214,24 @@ def b1040():
         print("Media: {:.1f}".format(media))
         print("Aluno reprovado.")
 
-
+def b1041():
+    x, y = map(float, input().split())
+    
+    if x == 0 and y == 0:
+        print("Origem")
+    elif x == 0:
+        print("Eixo Y")
+    elif y == 0:
+        print("Eixo X")
+    elif x > 0 and y > 0:
+        print("Q1")
+    elif x < 0 and y > 0:
+        print("Q2")
+    elif x < 0 and y < 0:
+        print("Q3")
+    elif x > 0 and y < 0:
+        print("Q4")
+    
 
 
 
