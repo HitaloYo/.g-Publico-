ad0d635a2c61f0c5988d376aa8984b45de613595
HitaloYo <76579977+HitaloYo@users.noreply.github.com>
2024-04-02 15:34:40 -0300

    Update TesteConta.java

--- TesteConta.java
+++ TesteConta.java
@@ -1,16 +1,15 @@
 public class TesteConta{
     public static void main(String[] args) {
         Conta minhaConta = new Conta("Fulano", 123, "001", 1000.0); // Criando uma instância da classe Conta
-    }
-    public static void main(String[] args) {
-        Conta minhaConta2 = new Conta("Fulano", 123, "001", 1000.0); // Criando uma instância da classe Conta
+        Conta minhaConta2 = new Conta("Fulano", 123, "001", 1000.0);
+        
+        if (minhaConta == minhaConta2) { //Sempre será falso pois apesar de terem atributos iguais são objt diferenstes
+            System.out.println("iguais");
+        } else {
+            System.out.println("diferentes");
+        }
     }
 
-    if(minhaConta == minhaConta2){
-        System.out.println("iguais");
-    }else{
-        System.out.println("diferentes");
-    }
     
     
 }
