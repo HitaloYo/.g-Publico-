54035911c3d06cacd78448525af8274a8132bafa
HitaloYo <76579977+HitaloYo@users.noreply.github.com>
2024-04-02 15:29:31 -0300

    Update TesteConta.java

--- TesteConta.java
+++ TesteConta.java
@@ -1,1 +1,16 @@
+public class TesteConta{
+    public static void main(String[] args) {
+        Conta minhaConta = new Conta("Fulano", 123, "001", 1000.0); // Criando uma instância da classe Conta
+    }
+    public static void main(String[] args) {
+        Conta minhaConta2 = new Conta("Fulano", 123, "001", 1000.0); // Criando uma instância da classe Conta
+    }
 
+    if(minhaConta == minhaConta2){
+        System.out.println("iguais");
+    }else{
+        System.out.println("diferentes");
+    }
+    
+    
+}
